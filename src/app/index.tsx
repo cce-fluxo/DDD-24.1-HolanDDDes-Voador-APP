a31dd5c04973dc40a8297fc701dfca2b5a979138
m-cf <marianafonseca@poli.ufrj.br>
2024-11-27 20:27:57 -0300

    add themes.tailwind - cores do proje

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Testando 123</Text>
+    <View>
+      <Text className="text-rosa-4">Testando 123</Text>
     </View>
   );
 }
