e7253bcd8614487c227a9d1eccfb819ea04d659c
Mariana Fonseca <marianafonseca@poli.ufrj.br>
2024-11-27 20:14:38 -0300

    add services.axios

---
+++ services/axios.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://ddd-24-1-holandddes-back-bk93.onrender.com/",
+  //baseURL: "http://localhost:3300/",
+  
+});
+
+api.interceptors.request.use((config: any) => {
+  const token = localStorage.getItem("@BonVoyage:token");
+  
+  if (token) {
+    if (config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+export default api;
