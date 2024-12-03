f0416d6105bf36e0b79f04842f954dcee4568038
m-cf <marianafonseca@poli.ufrj.br>
2024-12-02 21:05:45 -0300

    add configurando as cores e fontes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rosa-1": "#FFEDF0",
+        "rosa-2": "#F45065",
+        "rosa-3": "#F42C46",
+        "rosa-4": "#DC143B",
+        "preto": "#372F30",
+        "branco": "#FBFBFB",
+        "branco-2": "#F7F3F3",
+        "branco-3": "#D9D9D9",
+        "cinza-0": "#F6F4F4",
+        "cinza-1": "#CEC6C7",
+        "cinza-2": "#AB9C9F",
+        "cinza-3": "#574A4D",
+        "cinza-4": "#372F30",
+        "laranja": "#FD6944",
+        "laranja-1": "#FD765E",
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
+        readex_pro: ["var(--font-readex-pro)"],
+        work_sans: ["var(--font-work-sans)"],
+      },
+    },
   },
   plugins: [],
 };
