fcc336138a0e5933d67332f75873ab0fdec29eae
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2024-12-15 11:30:38 -0300

    ADD avaliacao

---
+++ app/(perfil)/avaliacao.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+import { View } from "react-native";
+
+export default function Avaliacao(){
+    return (
+        <View>
+            <Header titulo="Avaliações" className="w-full"/>
+        </View>
+    )
+}
