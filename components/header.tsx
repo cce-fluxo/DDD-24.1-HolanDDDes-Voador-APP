7c0952f1cae9a3a4a35ef2f58c1fcbe12665971d
coderatta <coderatta@gmail.com>
2024-12-17 15:40:26 -0300

    add modal

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,30 @@
-import { router } from "expo-router"
-import { TouchableOpacity, View, Text } from "react-native"
-import Svg, { Path } from "react-native-svg"
+import { router } from "expo-router";
+import { TouchableOpacity, View, Text } from "react-native";
+import Svg, { Path } from "react-native-svg";
 
 interface HeaderProps {
-    titulo: string;
-    className?: string; //estilizações adicionais caso sejam necessarias
+  titulo: string;
+  className?: string; //estilizações adicionais caso sejam necessarias
 }
 
-export default function Header( { titulo, className = "" }: HeaderProps) {
-    return (
-        <View className={`flex-row items-center px-4 py-3 bg-white ${className}`}>
-            <TouchableOpacity onPress={() => { router.back() }}>
-                <Svg width="12" height="20" viewBox="0 0 12 20" fill="none">
-                    <Path d="M11.835 1.8701L10.055 0.100098L0.165039 10.0001L10.065 19.9001L11.835 18.1301L3.70504 10.0001L11.835 1.8701Z" fill="#DC143B"/>
-                </Svg>
-            </TouchableOpacity>
-            <Text className="text-lg font-bold ml-4">{ titulo }</Text>
-        </View>
-    )
+export default function Header({ titulo, className = "" }: HeaderProps) {
+  return (
+    <View
+      className={`flex-row items-center px-4 py-4 bg-transparent gap-4 w-full ${className}`}
+    >
+      <TouchableOpacity
+        onPress={() => {
+          router.back();
+        }}
+      >
+        <Svg width="12" height="20" viewBox="0 0 12 20" fill="none">
+          <Path
+            d="M11.835 1.8701L10.055 0.100098L0.165039 10.0001L10.065 19.9001L11.835 18.1301L3.70504 10.0001L11.835 1.8701Z"
+            fill="#DC143B"
+          />
+        </Svg>
+      </TouchableOpacity>
+      <Text className="text-xl font-PoppinsMedium text-preto">{titulo}</Text>
+    </View>
+  );
 }
