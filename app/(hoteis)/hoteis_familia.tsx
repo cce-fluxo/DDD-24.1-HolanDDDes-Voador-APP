96cf5993162961d5c2065efa4fcc4b52b6ba1d90
Gustavo-Roxo <guga.amaralroxo@gmail.com>
2024-12-17 21:30:13 -0300

    home

--- app/(hoteis)/hoteis_familia.tsx
+++ app/(hoteis)/hoteis_familia.tsx
@@ -14,7 +14,7 @@ const HoteisFamilia = () => {
     return (
         <ScrollView>
             <View className=" mt-5 flex-col items-center gap-10 ">
-                <CardHotel nome="Pousada das Flores" lugar="Rio de Janeiro" preco={250} nota="4,2" comodidades={[1,1,0,0,0]} />
+                <CardHotel nome="Vasco" lugar="Rio de Janeiro" preco={750} nota="2,0" comodidades={[1,1,1,1,1]} />
                 <CardHotel nome="Palace" lugar="Sao Paulo" preco={200} nota="1,8" comodidades={[1,0,0,0,1]}/>
                 <CardHotel nome="Bom Sono" lugar="Rio de Janeiro" preco={500} nota="3,4" comodidades={[1,1,1,1,1]} />
             </View>
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,84 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { View, Text,Image, ImageBackground, ScrollView, TouchableOpacity } from "react-native";
+
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView >
+      <View className=" flex-col h-screen w-screen items-center">
+        <View className=" flex-row w-full h-[15%] justify-center items-center gap-3">
+          <Image
+          source={require("../../assets/images/logo.png")}
+          className="h-14 w-14"
+          resizeMode="contain"
+          />
+
+          <Text className=" font-medium text-2xl">
+            BonVoyage
+          </Text>
+          
+        </View>
+
+        <View className=" flex-row w-full h-20 justify-center">
+
+          <View className=" p-3 flex-row w-5/6 h-16 border rounded-3xl items-center justify-between">
+
+            <View className=" flex-row gap-3">
+              <Image
+              source={require("../../assets/images/lupa.png")}
+              className="h-8 w-8"
+              resizeMode="contain"
+              />
+
+              <View className="h-ful flex-col">
+                <Text >
+                  Qualquer lugar
+                </Text>
+                <Text className=" text-gray-500">
+                  Qualquer semana - Hóspedes?
+                </Text>
+              </View>
+            </View>
+
+            <TouchableOpacity>
+              <Image
+              source={require("../../assets/images/filtro.png")}
+              className="h-4 w-4"
+              resizeMode="contain"
+              />
+            </TouchableOpacity>
+          </View>
+
+        </View>
+
+        <View className=" flex-row w-5/6 h-[20%] mt-5 justify-around">
+
+          <TouchableOpacity>
+            <Text className=" font-semibold text-2xl">
+              Datas
+            </Text>
+
+            <Text>
+              
+            </Text>
+          </TouchableOpacity>
+
+          <View>
+            
+          </View>
+
+          <TouchableOpacity>
+            <Text>
+
+            </Text>
+
+            <Text>
+              
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </ScrollView>
   );
 }
